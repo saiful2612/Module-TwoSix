cbd726f7cd1452b433b936e965dd4ff200b632dd
saiful2612 <bristykodom@gmail.com>
2020-09-08 14:36:13 +0600

    4 module finished

--- callApply.js
+++ callApply.js
@@ -9,7 +9,12 @@ const normalPerson={
          console.log(this.firstName,this.lastName);
     },
     chargeBill: function(amount){
+        console.log(this);
         this.salary=this.salary-amount;
+        return this.salary;
+    },
+    houseRent: function(cost,serviceCharge,extra){
+        this.salary=this.salary-cost-serviceCharge-extra;
         return this.salary;
     }
 }
@@ -24,6 +29,42 @@ const normalPerson={
 
 // normalPerson.chargeBill(250);
 // console.log(normalPerson.salary);           //  Accessing method outside of the object
+
+// 26.3
+
+const heroPerson={
+    firstName:'Hero',
+    lastName: 'balam',
+    salary:2500
+}
+
+const frndlyPerson={
+    firstName:'Hero',
+    lastName: 'Golam',
+    salary:3000
+}
+
+// const heroChargeBill=normalPerson.chargeBill.bind(heroPerson);       // Apply of ' bind ' 
+// heroChargeBill(110);                                                 //  Call  new function         
+// console.log(heroPerson.salary);                
+
+
+// 26.4
+
+// normalPerson.chargeBill.call(frndlyPerson,340);                          // Apply of 'call'
+// normalPerson.chargeBill.call(heroPerson,500);
+// console.log(frndlyPerson.salary);
+// console.log(heroPerson.salary);
+
+
+// const savings= normalPerson.houseRent.call(heroPerson,600,100,30);
+// console.log(savings);          // console.log(heroPerson.salary) gives us same result
+
+
+// const deposit= normalPerson.houseRent.apply(frndlyPerson,[1500,250,40]);      // Apply of 'Apply'
+// console.log(deposit);
+
+
 
 
 
